d24b48f751aca624cb8e571f37bf50abdd826b8a
Mayank Chadha <mayank_chadha_chd@hotmail.com>
2022-05-17 11:14:38 +0530

    Changes for test Scenarios

--- Interview.Core/Repository.cs
+++ Interview.Core/Repository.cs
@@ -11,30 +11,28 @@ namespace Interview.Core
     {
 
 
-        private readonly IList<Person> personList = new List<Person>();
+        private readonly IList<T> _entityList;
+        public Repository(IList<T> entityList)
+        {
+            _entityList = entityList;
+            
+        }
 
         public void Add(T item)
         {
-           personList.Add((Entity)item as Person);
-            Console.WriteLine("Person Added");
+           _entityList.Add(item);
+           Console.WriteLine("Person Added");
         }
 
-        public IEnumerable<T> FindAll(Func<T, bool> where)
+        public IEnumerable<T> FindAll(Func<T, bool> x)
         {
-            return (IEnumerable<T>)personList.ToList();
+            return _entityList.Where(x);
         }
 
         public T Get(IComparable id)
-        {
-            
-            dynamic result= personList.Where(t => t.Id == id).FirstOrDefault(); 
-            if(result==null)
-            {
-                Console.WriteLine("No Results found");
-                return null;
-            }
-            else
-            return result;
+        {        
+           var result= _entityList.FirstOrDefault(t => t.Id.Equals(id)); 
+           return result;
         }
     }
 
--- Interview.Test/RepositoryTest.cs
+++ Interview.Test/RepositoryTest.cs
@@ -24,18 +24,36 @@ namespace Interview.Test
         //    _repository.Add(_person);
         //}
 
+      
+        private readonly Person _person = new Person();
+
         [Test]
 		public void AddTest_WhenSuccessful()
         {
-            var _person = new Person();
-            _person.Name = "New";
-            _person.Id = 22;
-            _person.Age = 26;
-            var _repository = new Repository<Person>();
-            _repository.Add(_person);
-            _repository.Add(_person);
-            Assert.IsNotNull(_person.Id);
+            List<Person> personList = new List<Person>();
+            var person = new Person { };
+            person.Name = "New";
+            person.Id = 22;
+            person.Age = 26;
+            var _repository = new Repository<Person>(personList);
+            _repository.Add(person);
+            Assert.AreEqual(personList.Count, 1);
         }
-		
+
+        [Test]
+        public void GetAll()
+        {
+            var persons = new List<Person>
+            { new Person{Name="Mayank",Id=1,Age=26},
+              new Person{Name="Ptern",Id=2,Age=23},
+              new Person{Name="3rd", Id=3, Age=19}
+            };
+
+            var _repository = new Repository<Person>(persons);
+            var results= _repository.FindAll(p =>p.Age >=20);
+            Assert.IsEmpty(results.Where(p=>p.Age<20));
+
+        }
+
     }
 }
